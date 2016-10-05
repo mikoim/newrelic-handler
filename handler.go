510ab4f9e4de8e5f719f85d5ce9df062f386be97
Eshin Kunishima <ek@esh.ink>
2016-10-05 21:41:21 +0900

    add comments

--- handler.go
+++ handler.go
@@ -6,15 +6,18 @@ import (
 	"github.com/newrelic/go-agent"
 )
 
+// Options contains application name and license key for New Relic.
 type Options struct {
 	ApplicationName string
 	LicenseKey      string
 }
 
+// NewRelic contains newrelic.Application.
 type NewRelic struct {
 	app *newrelic.Application
 }
 
+// New creates NewRelic populated with options.
 func New(options Options) (*NewRelic, error) {
 	var app newrelic.Application
 	app, err := newrelic.NewApplication(
@@ -29,6 +32,7 @@ func New(options Options) (*NewRelic, error) {
 	return &NewRelic{&app}, nil
 }
 
+// Handler collects running time.
 func (nr *NewRelic) Handler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		txn := (*nr.app).StartTransaction(r.URL.Path, w, r)
